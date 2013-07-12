db9dc4c9c92a66fdcb0cca17db55b24c4d9affcb
Jesús Zazueta <jjzazuet@gmail.com>
2013-07-12 00:25:36 -0500

    Package name departure.

---
+++ seeds-base/src/main/java/net/tribe7/common/base/package-info.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2007 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Basic utility libraries and interfaces.
+ *
+ * <p>This package is a part of the open-source
+ * <a href="http://guava-libraries.googlecode.com">Guava libraries</a>.
+ *
+ * <h2>Contents</h2>
+ *
+ * <h3>String-related utilities</h3>
+ *
+ * <ul>
+ * <li>{@link net.tribe7.common.base.Ascii}
+ * <li>{@link net.tribe7.common.base.CaseFormat}
+ * <li>{@link net.tribe7.common.base.CharMatcher}
+ * <li>{@link net.tribe7.common.base.Charsets}
+ * <li>{@link net.tribe7.common.base.Joiner}
+ * <li>{@link net.tribe7.common.base.Splitter}
+ * <li>{@link net.tribe7.common.base.Strings}
+ * </ul>
+ *
+ * <h3>Function types</h3>
+ *
+ * <ul>
+ * <li>{@link net.tribe7.common.base.Function},
+ *     {@link net.tribe7.common.base.Functions}
+ * <li>{@link net.tribe7.common.base.Predicate},
+ *     {@link net.tribe7.common.base.Predicates}
+ * <li>{@link net.tribe7.common.base.Equivalence}
+ * <li>{@link net.tribe7.common.base.Supplier},
+ *     {@link net.tribe7.common.base.Suppliers}
+ * </ul>
+ *
+ * <h3>Other</h3>
+ *
+ * <ul>
+ * <li>{@link net.tribe7.common.base.Defaults}
+ * <li>{@link net.tribe7.common.base.Enums}
+ * <li>{@link net.tribe7.common.base.Objects}
+ * <li>{@link net.tribe7.common.base.Optional}
+ * <li>{@link net.tribe7.common.base.Preconditions}
+ * <li>{@link net.tribe7.common.base.Stopwatch}
+ * <li>{@link net.tribe7.common.base.Throwables}
+ * </ul>
+ *
+ */
+@ParametersAreNonnullByDefault
+package net.tribe7.common.base;
+
+import javax.annotation.ParametersAreNonnullByDefault;
